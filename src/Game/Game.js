f881e343318753c166465072ef08ead4752b1593
Алексей <METEOP2024@yandex.ru>
2025-12-26 14:49:50 +0300

    I am creating the following components in the project

---
+++ src/Game/Game.js
@@ -0,0 +1,10 @@
+import { GameLayout } from './GameLayout'
+import './App.css';
+
+export const Game = () => {
+	return (
+		<>
+			<GameLayout />
+		</>
+	);
+};
---
+++ src/Game/GameLayout.js
@@ -0,0 +1,11 @@
+import { InformationContainer } from './Components/Information/InformationContainer';
+import { FieldContainer } from './Components/Field/FieldContainer';
+export const GameLayout = () => {
+	return (
+		<>
+			<div>Hello</div>
+			<InformationContainer />
+			<FieldContainer />
+		</>
+	);
+};
